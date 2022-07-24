9e69e73d92e6d0b5817da5ece18dd047cb1ae29b
choihan704 <choihan704@gmail.com>
2022-07-25 03:35:02 +0900

    first commit

---
+++ quotes.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:password@localhost/quotes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bgxfydqzvnvhee:737860bce056fade50d32ecf235f955ebe36537fae785ab87c5b11b9d82d5f53@ec2-54-152-28-9.compute-1.amazonaws.com:5432/dc61fetp95p5mc'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Favquotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    quote = db.Column(db.String(2000))
+
+@app.route('/')
+def index():
+    result = Favquotes.query.all()
+    return render_template('index.html', result=result)
+
+@app.route('/about')
+def about():
+    return '<h1>Hello World from About</h1>'
+
+@app.route('/quotes')
+def quotes():
+    return render_template('quotes.html')
+
+@app.route('/process', methods = ['POST'])
+def process():
+    author = request.form['author']
+    quote = request.form['quote']
+    quotedata = Favquotes(author=author, quote=quote)
+    db.session.add(quotedata)
+    db.session.commit()
+
+    return redirect(url_for('index'))
